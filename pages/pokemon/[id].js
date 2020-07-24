45d153af633050de9e929259f6987942638ed0a2
CrypticMick <60044342+CrypticMick@users.noreply.github.com>
2020-07-24 13:27:59 -0400

    example with getServerSideProps

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -2,6 +2,34 @@ import { Heading, Box, Text } from '@chakra-ui/core'
 import { posts } from '../../data/posts'
 import { useRouter } from 'next/router'
 
-export default function Post() {
-  return <div></div>
+export default function PokemonCharacter(props) {
+  console.log(props)
+  const { data } = props
+  if (!data) {
+    return (
+      <Box>
+        <Text>No pokemon found</Text>
+      </Box>
+    )
+  }
+
+  return (
+    <Box>
+      <Text>{data.name}</Text>
+    </Box>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const {
+    params: { id },
+  } = context
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}/`
+
+  const res = await fetch(url)
+  const data = await res.json()
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
 }
