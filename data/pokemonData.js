78ba5fc6b4cffbb62727e3eddcb9feed9bc9e5f8
CrypticMick <60044342+CrypticMick@users.noreply.github.com>
2020-07-24 12:14:25 -0400

    work in progress getInitialProps

---
+++ data/pokemonData.js
@@ -0,0 +1,50 @@
+export const pokemonData = [
+  {
+    id: '1',
+    name: 'Bulbasaur',
+  },
+  {
+    id: '2',
+    name: 'Ivysaur',
+  },
+  {
+    id: '3',
+    name: 'Venusaur',
+  },
+  {
+    id: '4',
+    name: 'Charmander',
+  },
+  {
+    id: '5',
+    name: 'Charmeleon',
+  },
+  {
+    id: '6',
+    name: 'Charizard',
+  },
+  {
+    id: '7',
+    name: 'Squirtle',
+  },
+  {
+    id: '8',
+    name: 'Wartortle',
+  },
+  {
+    id: '9',
+    name: 'Blastoise',
+  },
+  {
+    id: '10',
+    name: 'Caterpie',
+  },
+  {
+    id: '11',
+    name: 'Metapod',
+  },
+  {
+    id: '12',
+    name: 'Butterfree',
+  },
+]
---
+++ pages/pokemon/[id].js
@@ -0,0 +1,7 @@
+import { Heading, Box, Text } from '@chakra-ui/core'
+import { posts } from '../../data/posts'
+import { useRouter } from 'next/router'
+
+export default function Post() {
+  return <div></div>
+}
